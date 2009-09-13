3d13d4c3ae24f9b530e56d440a302a8babfc04be
Brian Muller <bamuller@gmail.com>
2009-09-13 20:43:30 +0000

    working on installable package

--- R/collection.R
+++ R/collection.R
@@ -10,12 +10,14 @@ collection <- function(size=0, default=NA) {
 }
 
 collection.get <- function(col, key, drop=TRUE) {
-  varname <- paste('vars.', key)
+  key <- valid.key(col, key)
+  varname <- paste('vars', key, sep=".")
   return(get(varname, envir=col@env))
 }
 
 collection.set <- function(col, key, value) {
-  varname <- paste('vars.', key)  
+  key <- valid.key(col, key)
+  varname <- paste('vars', key, sep=".")  
   assign(varname, value, envir=col@env)
   return(col)
 }
@@ -26,7 +28,7 @@ valid.key <- function(col, key) {
   key <- as.integer(key)
   if(is.na(key))
     stop("Cannot coerce index object into an integer")
-  if(key > length(col))
+  if(key > length(col) || key == 0)
     stop("Index out of bounds")
   return(key)
 }
@@ -41,7 +43,6 @@ setReplaceMethod("[", signature(x="collection", i="ANY", j="missing", value="ANY
                      index <- as.integer(length(x) + 1)
                      return(collection.set(x, index, value))
                    }
-                   i <- valid.key(x, i)
                    return(collection.set(x, i, value))
                  }
                  )
@@ -50,6 +51,11 @@ setMethod("length", "collection", function(x) length(x@env))
 
 setMethod("show", "collection", function(object) {
   cat("A 'collection' with a size of", length(object))
+})
+
+setGeneric("clear", function(x) standardGeneric("clear"))
+setMethod("clear", "collection", function(x) {
+  rm(list=paste("vars", 1:length(x), sep="."), envir=x@env)
 })
 
 is.collection <- function(x) is( x, "collection" )
